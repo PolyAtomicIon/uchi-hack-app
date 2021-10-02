3edf597e58746dd71b4f6c27419898bf5e5dfd0c
Bekzat Yernat <180107265@stu.sdu.edu.kz>
2021-10-02 11:30:09 +0600

    q-list is sortable

--- src/pages/GamesList.vue
+++ src/pages/GamesList.vue
@@ -1,133 +1,155 @@
 <template>
-  <q-page class="q-pa-sm">
-  </q-page>
+  <div class="row">
+    <div class="col-1">
+      <button class="btn btn-secondary button" @click="add">Add</button>
+    </div>
+
+    <div class="col-7">
+      <h3>Draggable {{ draggingInfo }}</h3>
+
+      <draggable
+        tag="ul"
+        :list="list"
+        v-bind="dragOptions"
+        class="list-group"
+        handle=".handle"
+        item-key="name"
+      >
+        <template #item="{ element, index }">
+          <li class="list-group-item">
+            <button class="handle"></button>
+              {{ element.name }}
+            <q-form
+              @submit="onSubmit"
+              @reset="onReset"
+              class="q-gutter-md question-form"
+            >
+              <q-select filled v-model="model" :options="options" label="Label (stacked)" stack-label :dense="dense" :options-dense="denseOpts" />
+
+              <q-input
+                filled
+                v-model="element.text"
+                label="Your name *"
+                hint="Name and surname"
+                lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Please type something']"
+              />
+
+
+              <div>
+                <q-btn label="Submit" type="submit" color="primary"/>
+                <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+                <q-btn label="Delete" type="reset" color="red" flat class="q-ml-sm" @click="removeAt(index)" />
+              </div>
+            </q-form>
+          </li>
+        </template>
+      </draggable>
+    </div>
+
+    <rawDisplayer class="col-3" :value="list" title="List" />
+  </div>
 </template>
 
 <script>
-  export default {
-      data() {
-          return {
-              filter: "",
-              customer: {},
-              new_customer: false,
-              mode: "list",
-              columns: [
-                  {
-                      name: "name",
-                      required: true,
-                      label: "Customer Name",
-                      align: "left",
-                      field: "name",
-                      sortable: true
-                  },
-                  {
-                      name: "city",
-                      align: "left",
-                      label: "City",
-                      field: "city",
-                      sortable: true
-                  },
-                  {
-                      name: "state",
-                      align: "left",
-                      label: "State",
-                      field: "state",
-                      sortable: true
-                  },
-                  {
-                      name: "last_call",
-                      align: "left",
-                      label: "Last Call",
-                      field: "last_call",
-                      sortable: true
-                  }
-              ],
-              data: [
-                  {
-                      name: "Dr. Jada Conolly",
-                      city: "GILBERT",
-                      state: "AZ",
-                      last_call: "12-09-2019"
-                  },
-                  {
-                      name: "Dr. Kiley Ibbotson",
-                      city: "LA MESA",
-                      state: "CA",
-                      last_call: "09-02-2019"
-                  },
-                  {
-                      name: "Dr. Leslie Tecklenburg",
-                      city: "SAN DIEGO",
-                      state: "CA",
-                      last_call: "03-25-2019"
-                  },
-                  {
-                      name: "Dr. Lia Whitledge",
-                      city: "PHOENIX",
-                      state: "AZ",
-                      last_call: "03-18-2019"
-                  },
-                  {
-                      name: "Dr. Sam Wileman",
-                      city: "MESA",
-                      state: "AZ",
-                      last_call: "04-09-2019"
-                  },
-                  {
-                      name: "Dr. Edgar Colmer",
-                      city: "PHOENIX",
-                      state: "AZ",
-                      last_call: "09-03-2019"
-                  },
-                  {
-                      name: "Dr. Kaiden Rozelle",
-                      city: "LAKEWOOD",
-                      state: "CA",
-                      last_call: "01-12-2019"
-                  },
-                  {
-                      name: "Dr. Leslie Stopher",
-                      city: "YUMA",
-                      state: "AZ",
-                      last_call: "04-15-2019"
-                  },
-                  {
-                      name: "Dr. Miguel Subasic",
-                      city: "TEMPE",
-                      state: "AZ",
-                      last_call: "11-09-2019"
-                  },
-                  {
-                      name: "Dr. Reese Vandygriff",
-                      city: "LAKEWOOD",
-                      state: "CA",
-                      last_call: "01-01-2019"
-                  },
-                  {
-                      name: "Dr. Griffin Troglen",
-                      city: "YUMA",
-                      state: "AZ",
-                      last_call: "04-12-2019"
-                  },
-                  {
-                      name: "Dr. Zachary Wehrley",
-                      city: "TEMPE",
-                      state: "AZ",
-                      last_call: "10-09-2019"
-                  },
-                  {
-                      name: "Dr. Kyle Wahlert",
-                      city: "LAKEWOOD",
-                      state: "CA",
-                      last_call: "01-02-2019"
-                  }
-              ],
-              pagination: {
-                  rowsPerPage: 10
-              }
-          };
-      },
-      methods: {
-      }
-  };
+import draggable from "vuedraggable";
+import { ref } from 'vue'
+let id=3;
+export default {
+  name: "handle",
+  display: "Handle",
+  instruction: "Drag using the handle icon",
+  order: 5,
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      list: [
+        { name: "John", text: "", id: 0 },
+        { name: "Joao", text: "", id: 1 },
+        { name: "Jean", text: "", id: 2 }
+      ],
+      dragging: false
+    };
+  },
+  setup () {
+    return {
+      model: ref(null),
+
+      options: [
+        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+      ],
+
+      dense: ref(false),
+      denseOpts: ref(false)
+    }
+  },
+  computed: {
+    draggingInfo() {
+      return this.dragging ? "under drag" : "";
+    },
+    dragOptions() {
+      return {
+        animation: 200,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost-ticket",
+        chosenClass: "chosen-ticket",
+        dragClass: "dragging-ticket",
+      };
+    }
+  },
+  methods: {
+    removeAt(idx) {
+      this.list.splice(idx, 1);
+    },
+    add: function() {
+      id++;
+      this.list.push({ name: "Juan " + id, id, text: "" });
+    },
+  }
+};
 </script>
+<style lang="scss" scoped>
+
+.handle {
+  float: left;
+  padding-top: 8px;
+  padding-bottom: 8px;
+}
+
+.list-group {
+  list-style: none;
+  &-item {
+    margin: 12px;
+    padding: 12px;
+    border: 1px solid red;
+    background: lightcoral;
+    border-radius: 1px;
+  }
+}
+
+.question-form {
+  background: #fff;
+  margin: 12px;
+  padding: 12px;  
+}
+
+.chosen-ticket {
+    background: darkorange !important;
+    opacity: 0;
+}
+.dragging-ticket {
+    background: darkred !important;
+    background-color: darkred !important;
+    opacity: 1 !important;
+    box-shadow: none !important;
+    border: 2px solid black;
+}
+
+.ghost-ticket {
+  opacity: 1 !important;
+}
+
+</style>
